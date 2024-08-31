b2e68c2b36f3c58f63da456d00fbb6d4752cfd21
Neha Singh <singhneha252503@gmail.com>
2024-08-31 14:34:54 +0000

    some changes

--- .devcontainer/01_Basics/datatype_summary.js
+++ .devcontainer/01_Basics/datatype_summary.js
@@ -10,3 +10,25 @@ const id = Symbol('123'); //unique
 const id1 = Symbol('123');
 
 console.log(id === id1);
+
+// +++++++++++++++ MEMORY +++++++++++++++++++++++++++
+
+// STACK (Primitive)  -> copy milti hai.
+//ex-
+let myName = "Neha";
+let newName = myName;
+newName = "Visha";
+console.log(myName);
+console.log(newName);
+
+
+
+// HEAP (Non-Primitive)  -> refernce milta hai.(jo bhi chnage karenge wo original value me change hoga.)
+//ex-
+let user1 = {
+    name :"neha",
+    age :21
+}
+let user2 = user1;
+user2.age = 20;
+console.log(user1.age);
