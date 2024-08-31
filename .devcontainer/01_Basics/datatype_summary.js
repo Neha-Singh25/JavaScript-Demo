fd3c9b6cef10f99ae8bb72a410daba425c83247f
Neha Singh <singhneha252503@gmail.com>
2024-08-31 14:21:35 +0000

    datatype summary

---
+++ .devcontainer/01_Basics/datatype_summary.js
@@ -0,0 +1,12 @@
+// PRIMITIVE
+
+// 7 types : String, Number, Boolean , null, undefined , Symbol, BigInt
+
+//REFERENCE (NON-PRIMITIVE)
+
+// Array , Objects , Functions 
+
+const id = Symbol('123'); //unique
+const id1 = Symbol('123');
+
+console.log(id === id1);
