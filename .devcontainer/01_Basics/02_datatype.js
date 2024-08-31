e306f001722a14bf44ed030a17a9d80ed71b5dca
Neha Singh <singhneha252503@gmail.com>
2024-08-31 12:54:49 +0000

    add datatype

---
+++ .devcontainer/01_Basics/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+// alert (3+3)  we are using nodde.js , not browser
+
+let name = "Neha"
+let number = 133
+let islogin = true
+
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Neha");
+console.log(typeof number);
+console.log(typeof null);  //object
+console.log(typeof undefined); //undefined
+
