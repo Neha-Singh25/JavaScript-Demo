cc8811642e2225fbfc92f2169e5dc86f19522b37
Neha Singh <singhneha252503@gmail.com>
2024-08-31 13:14:36 +0000

    add converstion

---
+++ .devcontainer/01_Basics/03_converstion.js
@@ -0,0 +1,25 @@
+// let num = "33abc";
+// let num = null;   
+// let num = undefined;
+
+// console.log(typeof num)
+// let value = Number (num)  //convert number
+// console.log(typeof value);
+// console.log(value);   //NaN (33abc, undefined),   0(null) 
+
+/*
+    "33" => 33
+    "33abc" => NaN
+    true => 1; false =>0
+    null => 0; undefined => NaN
+    neha => NaN
+*/
+
+// let isLogin = 1; 
+// let result = Boolean(isLogin);
+// console.log(result);
+
+// 1 =>true   0=>false
+//"" =>false
+// "Neha" => true
+
